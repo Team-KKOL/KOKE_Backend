3755049f287902803e571dc3a74cd7207ff63103
habin <danny9643@naver.com>
2022-12-23 16:07:25 +0900

    initializer, ValidEnumValidator 모듈 추가

--- src/main/java/com/koke/koke_backend/module/json/serializer/ResponseDtoSerializer.java
+++ src/main/java/com/koke/koke_backend/module/json/serializer/ResponseDtoSerializer.java
@@ -1,9 +1,9 @@
 package com.koke.koke_backend.module.json.serializer;
 
-import com.bpnsolution.blockchain_exchange.dto.ResponseDto;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.koke.koke_backend.dto.ResponseDto;
 
 import java.io.IOException;
 
