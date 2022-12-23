19641015aedf059c92650eed58d548ffa252906c
habin <danny9643@naver.com>
2022-12-23 13:30:42 +0900

    first commit

---
+++ src/main/java/com/koke/koke_backend/KokeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.koke.koke_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koke/koke_backend/KokeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koke.koke_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
