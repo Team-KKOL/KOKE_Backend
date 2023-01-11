9c2118c8b3c04537575d4a522261efe673aab390
habin <danny9643@naver.com>
2023-01-11 10:20:05 +0900

    OauthProviderType에서 Facebook 삭제

--- src/main/java/com/koke/koke_backend/common/security/oauth/info/OAuth2UserInfoFactory.java
+++ src/main/java/com/koke/koke_backend/common/security/oauth/info/OAuth2UserInfoFactory.java
@@ -1,6 +1,5 @@
 package com.koke.koke_backend.common.security.oauth.info;
 
-import com.koke.koke_backend.common.security.oauth.info.impl.FacebookOAuth2UserInfo;
 import com.koke.koke_backend.common.security.oauth.info.impl.GoogleOAuth2UserInfo;
 import com.koke.koke_backend.common.security.oauth.info.impl.KakaoOAuth2UserInfo;
 import com.koke.koke_backend.common.security.oauth.info.impl.NaverOAuth2UserInfo;
@@ -11,7 +10,6 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
         return switch (providerType) {
             case GOOGLE -> new GoogleOAuth2UserInfo(attributes);
-            case FACEBOOK -> new FacebookOAuth2UserInfo(attributes);
             case NAVER -> new NaverOAuth2UserInfo(attributes);
             case KAKAO -> new KakaoOAuth2UserInfo(attributes);
             default -> throw new IllegalArgumentException("Invalid Provider Type.");
